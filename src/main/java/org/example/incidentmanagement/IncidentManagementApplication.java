015e8e5ad712021605dd55aa6e937c2b0d37a123
Natena777 <nikoloz.natenadze.2@gmail.com>
2025-12-30 08:45:32 +0000

    add logging

--- src/main/java/org/example/incidentmanagement/IncidentManagementApplication.java
+++ src/main/java/org/example/incidentmanagement/IncidentManagementApplication.java
@@ -1,7 +1,5 @@
 package org.example.incidentmanagement;
 
-import java.time.LocalDateTime;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
