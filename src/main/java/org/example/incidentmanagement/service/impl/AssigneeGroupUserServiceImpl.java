f2edb11ecfbf237e2f5668d5362d50c3b9fca3eb
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-04 02:46:50 +0400

    Update AssigneeGroupUserServiceImpl.java

--- src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
+++ src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AssigneeGroupUserServiceImpl implements AssigneeGroupUsersService {
+public class AssigneeGroupUserServiceImpl implements AssigneeGroupUserService {
 
     private final AssigneeGroupUsersRepository assigneeGroupUsersRepository;
     private final Logger logger = LoggerFactory.getLogger(AssigneeGroupUserServiceImpl.class);
