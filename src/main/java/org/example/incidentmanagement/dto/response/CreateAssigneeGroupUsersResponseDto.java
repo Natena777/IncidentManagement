e355940d146c67c21515dcfc87f1c88e2734675c
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-03 21:25:20 +0000

    Commit

--- src/main/java/org/example/incidentmanagement/dto/response/CreateAssigneeGroupUsersResponseDto.java
+++ src/main/java/org/example/incidentmanagement/dto/response/CreateAssigneeGroupUsersResponseDto.java
@@ -5,10 +5,26 @@ public class CreateAssigneeGroupUsersResponseDto {
     private Integer id;
     private Integer userId;
     private Integer assigneeGroupId;
-    private String active;
-    private Integer createdBy;
-    private LocalDateTime createdOn;
-    private Integer updatedBy;
-    private LocalDateTime updatedOn;
 
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getUserId() {
+        return userId;
+    }
+    public void setUserId(Integer userId){
+        this.userId = userId;
+    }
+    public Integer getAssigneeGroupId(){
+        return assigneeGroupId;
+    }
+    public void setAssigneeGroupId(Integer assigneeGroupId){
+        this.assigneeGroupId = assigneeGroupId;
+    }
+    
 }
