d776104052004a7dc15e731879b45f84e77ce04f
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-04 01:56:48 +0400

    Development (#23)
    * add command files
    
    * add git commands file
    
    * add git commands file
    
    * Add Front List
    
    * Add Front List
    
    * Add Front List
    
    * Add Front List
    
    * Add Front List
    
    * Add Front List
    
    * Fix User roles Delete List
    
    * Fix User roles Front Selectors
    
    * modify back logic
    
    * Change Visual
    
    * Refactor CSS styles for improved layout and design
    
    * Change Mapper logics
    
    * Add Assignee Groups Users Management
    
    * Fix Class Type
    
    * Fix Imports Bug
    
    * Fix Class Type Bug (#17)
    
    * Commit
    
    * fix bugs
    
    * Last Commit
    
    * Rename
    
    * Rename

--- src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
+++ src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
@@ -4,21 +4,21 @@ import org.example.incidentmanagement.service.CurrentUserService;
 import org.example.incidentmanagement.dto.requests.CreateAssigneeGroupUsersRequestDto;
 import org.example.incidentmanagement.dto.response.AssigneeGroupUsersResponseDto;
 import org.example.incidentmanagement.dto.response.CreateAssigneeGroupUsersResponseDto;
-import org.example.incidentmanagement.mapper.AssigneeGroupUsersMapper;
+import org.example.incidentmanagement.mappers.AssigneeGroupUsersMapper;
 import org.example.incidentmanagement.repository.AssigneeGroupUsersRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AssigneeGroupUserServiceimpl implements AssigneeGroupUserService {
+public class AssigneeGroupUserServiceImpl implements AssigneeGroupUserService {
 
     private final AssigneeGroupUsersRepository assigneeGroupUsersRepository;
-    private final Logger logger = LoggerFactory.getLogger(AssigneeGroupUserServiceimpl.class);
+    private final Logger logger = LoggerFactory.getLogger(AssigneeGroupUserServiceImpl.class);
     private final AssigneeGroupUsersMapper assigneeGroupUsersMapper;
     private final CurrentUserService currentUserService;
 
-    public AssigneeGroupUserServiceimpl(AssigneeGroupUsersRepository assigneeGroupUsersRepository, AssigneeGroupUsersMapper assigneeGroupUsersMapper,
+    public AssigneeGroupUserServiceImpl(AssigneeGroupUsersRepository assigneeGroupUsersRepository, AssigneeGroupUsersMapper assigneeGroupUsersMapper,
                                     CurrentUserService currentUserService) {
         this.assigneeGroupUsersRepository = assigneeGroupUsersRepository;
         this.assigneeGroupUsersMapper = assigneeGroupUsersMapper;
