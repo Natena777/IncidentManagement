e405f086cdd69a76f3ebfca52fcca5a902148b50
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-03 22:19:27 +0000

    Commit

--- src/main/java/org/example/incidentmanagement/controller/AssigneeGroupUsersController.java
+++ src/main/java/org/example/incidentmanagement/controller/AssigneeGroupUsersController.java
@@ -2,6 +2,7 @@ package org.example.incidentmanagement.controller;
 
 import org.example.incidentmanagement.dto.response.CreateAssigneeGroupUsersResponseDto;
 import org.example.incidentmanagement.entity.AssigneeGroupUsers;
+import org.example.incidentmanagement.service.AssigneeGroupUsersService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,23 +18,23 @@ import org.example.incidentmanagement.dto.response.AssigneeGroupUsersResponseDto
         description = "Manage Assignee Group Users In System")
 public class AssigneeGroupUsersController {
 
-    private final AssigneeGroupUsers assigneeGroupUsers;
+    private final AssigneeGroupUsersService assigneeGroupUsersService;
 
-    public AssigneeGroupUsersController(AssigneeGroupUsers assigneeGroupUsers) {
-        this.assigneeGroupUsers = assigneeGroupUsers;
+    public AssigneeGroupUsersController(AssigneeGroupUsersService assigneeGroupUsersService) {
+        this.assigneeGroupUsersService = assigneeGroupUsersService;
     }
 
     @PostMapping("/addUsers")
     @Operation(summary = "add Users to Assignee Group")
     public CreateAssigneeGroupUsersResponseDto createAssigneeGroup(@RequestBody CreateAssigneeGroupRequestDto createAssigneeGroupRequestDto){
-        CreateAssigneeGroupUsersResponseDto result = assigneeGroupUsers.createAssigneeGroup(createAssigneeGroupRequestDto);
+        CreateAssigneeGroupUsersResponseDto result = assigneeGroupUsersService.addUserInAssigneeGroup(createAssigneeGroupRequestDto);
         return result;
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Remove User From Assignee Group By ID")
     public ResponseEntity<ApiResponse> deleteAssigneeGroupUsers(@PathVariable Integer id) {
-        assigneeGroupUsers.deleteAssigneeGroupUsers(id);
+        assigneeGroupUsersService.removeUserFromAssigneeGroup(id);
         return ResponseEntity.ok(new ApiResponse("Assignee Group Users deleted successfully"));
     }
 
@@ -41,7 +42,7 @@ public class AssigneeGroupUsersController {
     @GetMapping("/id/{id}")
     @Operation(summary = "Find Assignee Group Users By ID")
     public AssigneeGroupUsersResponse findById(@PathVariable Integer id) {
-        return assigneeGroupUsers.findById(id);
+        return assigneeGroupUsersService.findById(id);
     }  
     
 }
