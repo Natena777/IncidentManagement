4bdc95b38d1a9a8cf7706314f022262bed9eabcd
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-04 13:38:27 +0000

    Add Case Status Name Getter

--- src/main/java/org/example/incidentmanagement/repository/CaseStatusesRepository.java
+++ src/main/java/org/example/incidentmanagement/repository/CaseStatusesRepository.java
@@ -2,6 +2,7 @@ package org.example.incidentmanagement.repository;
 
 import org.example.incidentmanagement.entity.CaseStatuses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -9,5 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface CaseStatusesRepository extends JpaRepository<CaseStatuses, Integer> {
 
     CaseStatuses findBystatusName(String statusName);
+
+    @Query("select cs.statusName from CaseStatuses cs where cs.id = :id")
+    String getNameById(Integer id);
 
 }
