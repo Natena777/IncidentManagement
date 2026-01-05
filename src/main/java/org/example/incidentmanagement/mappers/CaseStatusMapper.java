77910360feb896f2eb634d02c5678193e355d2f0
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-05 19:34:13 +0000

    Case Status Refactor

--- src/main/java/org/example/incidentmanagement/mappers/CaseStatusMapper.java
+++ src/main/java/org/example/incidentmanagement/mappers/CaseStatusMapper.java
@@ -8,31 +8,41 @@ import org.mapstruct.Mapper;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DefaultConverter.class})
 public interface CaseStatusMapper {
 
 
     //Default Response Of Case statuses
+    @Mapping(source = "createdBy", target = "createdBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "updatedBy", target = "updatedBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "isFinal", target = "isFinal", qualifiedByName = "stringToBoolean")
+    @Mapping(source = "isPaused", target = "isPaused", qualifiedByName = "stringToBoolean")
     CaseStatusesResponseDto toCaseStatusResponseDto(CaseStatuses caseStatuses);
 
 
 
     //Create Case Statuses Request And Responses
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedOn", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "active", constant = "Y")
+    @Mapping(source = "isFinal", target = "isFinal", qualifiedByName = "booleanToString")
+    @Mapping(source = "isPaused", target = "isPaused", qualifiedByName = "booleanToString")
     CaseStatuses toCaseStatusEntity(CreateCaseStatusesRequestDto createCaseStatusesRequestDto);
+        default CaseStatuses toCaseStatusEntityDefaults(CreateCaseStatusesRequestDto createCaseStatusesRequestDto, 
+                                                        Integer currentUserId){
+            CaseStatuses entity = toCaseStatusEntity(createCaseStatusesRequestDto);
+            entity.setCreatedBy(currentUserId);
+            entity.setCreatedOn(LocalDateTime.now(ZoneId.of("Asia/Tbilisi")));
+            return entity;
+        }
 
-    default CaseStatuses toCaseStatusEntityDefaults(CreateCaseStatusesRequestDto createCaseStatusesRequestDto, 
-                                                    Integer currentUserId,
-                                                    String isFinal,
-                                                    String isPaused){
-        CaseStatuses entity = toCaseStatusEntity(createCaseStatusesRequestDto);
-        entity.setActive("Y");
-        entity.setCreatedBy(currentUserId);
-        entity.setCreatedOn(LocalDateTime.now(ZoneId.of("Asia/Tbilisi")));
-        entity.setIsFinal(isFinal);
-        entity.setIsPaused(isPaused);
-        return entity;
-    }
-
+    @Mapping(source = "createdBy", target = "createdBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "updatedBy", target = "updatedBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "isFinal", target = "isFinal", qualifiedByName = "stringToBoolean")
+    @Mapping(source = "isPaused", target = "isPaused", qualifiedByName = "stringToBoolean")
     CreateCaseStatusesResponseDto toCreateCaseStatusResponseDto(CaseStatuses caseStatuses);
 
 
