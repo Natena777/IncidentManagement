7c8b444244c36fef470ad3afb4efc752e7e53415
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-05 18:52:43 +0000

    Assignee Group Users Refactor

--- src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
+++ src/main/java/org/example/incidentmanagement/service/impl/AssigneeGroupUserServiceImpl.java
@@ -1,7 +1,6 @@
 package org.example.incidentmanagement.service.impl;
 
 import org.example.incidentmanagement.service.CurrentUserService;
-import org.example.incidentmanagement.service.DefaultConverter;
 import org.example.incidentmanagement.entity.AssigneeGroupUsers;
 import org.example.incidentmanagement.service.AssigneeGroupUserService;
 import org.example.incidentmanagement.dto.requests.CreateAssigneeGroupUsersRequestDto;
@@ -20,14 +19,12 @@ public class AssigneeGroupUserServiceImpl implements AssigneeGroupUserService {
     private final Logger logger = LoggerFactory.getLogger(AssigneeGroupUserServiceImpl.class);
     private final AssigneeGroupUsersMapper assigneeGroupUsersMapper;
     private final CurrentUserService currentUserService;
-    private final DefaultConverter defaultConverter;
 
     public AssigneeGroupUserServiceImpl(AssigneeGroupUsersRepository assigneeGroupUsersRepository, AssigneeGroupUsersMapper assigneeGroupUsersMapper,
-                                    CurrentUserService currentUserService, DefaultConverter defaultConverter) {
+                                    CurrentUserService currentUserService) {
         this.assigneeGroupUsersRepository = assigneeGroupUsersRepository;
         this.assigneeGroupUsersMapper = assigneeGroupUsersMapper;
         this.currentUserService = currentUserService;
-        this.defaultConverter = defaultConverter;
     }
 
 
@@ -36,12 +33,7 @@ public class AssigneeGroupUserServiceImpl implements AssigneeGroupUserService {
 
         AssigneeGroupUsers groupUsers = assigneeGroupUsersRepository.findById(id).orElse(null);
         
-        String fullName = defaultConverter.getUserFullName(groupUsers.getUserId());
-        String groupName = defaultConverter.getAssigneeGroupName(groupUsers.getAssigneeGroupId());
-        String createdBy = defaultConverter.getUserFullName(groupUsers.getCreatedBy());
-        String updatedBy = defaultConverter.getUserFullName(groupUsers.getUpdatedBy());
-
-        AssigneeGroupUsersResponseDto responseResult = assigneeGroupUsersMapper.toResponseGroupUsers(groupUsers, fullName, groupName, createdBy, updatedBy); 
+        AssigneeGroupUsersResponseDto responseResult = assigneeGroupUsersMapper.toResponseGroupUsers(groupUsers); 
 
         return responseResult;
     }
@@ -54,10 +46,7 @@ public class AssigneeGroupUserServiceImpl implements AssigneeGroupUserService {
         
         assigneeGroupUsersRepository.save(assigneeGroupUsers);
 
-        String fullName = defaultConverter.getUserFullName(assigneeGroupUsers.getUserId());
-        String groupName = defaultConverter.getAssigneeGroupName(assigneeGroupUsers.getAssigneeGroupId());
-
-        return assigneeGroupUsersMapper.toResponseCrGroupUsers(assigneeGroupUsers, fullName, groupName);
+        return assigneeGroupUsersMapper.toResponseCrGroupUsers(assigneeGroupUsers);
     }
 
     @Override
