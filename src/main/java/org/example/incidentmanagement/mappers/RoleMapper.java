0cee0aca16d5799a375dfd8260b04688f168a7a5
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-05 19:48:56 +0000

    Role Mapper Refactor

--- src/main/java/org/example/incidentmanagement/mappers/RoleMapper.java
+++ src/main/java/org/example/incidentmanagement/mappers/RoleMapper.java
@@ -8,10 +8,14 @@ import org.mapstruct.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DefaultConverter.class})
 public interface RoleMapper {
 
     @Mapping(source = "name", target = "role")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "createdBy", target = "createdBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "updatedBy", target = "updatedBy", qualifiedByName = "userIdToFullName")
     RoleResponseDto toRoleResponseDto(Role role);
 
 
@@ -19,20 +23,23 @@ public interface RoleMapper {
     @Mapping(source = "newRoleName", target = "name")
     @Mapping(source = "newRoleDescription", target = "description")
     @Mapping(source = "newRoleStatus", target = "status")
-    
     void updateFromDto(UpdateRoleRequestDto dto,
                        @MappingTarget Role role);
 
     
     @Mapping(source = "roleName", target = "name")
     @Mapping(source = "roleDescription", target = "description")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedOn", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "status", constant = "A")
     Role toEntity(RoleRequestDto roleRequestDto);
-
     default Role toEntityDetails(RoleRequestDto roleRequestDto, Integer currentUserId){
         Role entity = toEntity(roleRequestDto);
         entity.setCreatedBy(currentUserId);
         entity.setCreatedOn(LocalDateTime.now(ZoneId.of("Asia/Tbilisi")));
-        entity.setStatus("A");
         return entity;
     }
 
