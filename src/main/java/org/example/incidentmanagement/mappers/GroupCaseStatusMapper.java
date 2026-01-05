f7f0de05478b66afde9e47b11122c2fac4c86704
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-05 19:44:28 +0000

    Case Status Refactor

--- src/main/java/org/example/incidentmanagement/mappers/GroupCaseStatusMapper.java
+++ src/main/java/org/example/incidentmanagement/mappers/GroupCaseStatusMapper.java
@@ -9,10 +9,16 @@ import org.mapstruct.Mapper;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {DefaultConverter.class})
 public interface GroupCaseStatusMapper {
 
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedOn", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "active", constant = "A")
     AssigneeGroupCaseStatus toGroupCaseStatusEntity(CrGroupCaseStatRequestDto crGroupCaseStatRequestDto);
         default AssigneeGroupCaseStatus toGroupCaseStatusEntityDefaults (CrGroupCaseStatRequestDto crGroupCaseStatRequestDto, Integer currentUserId){
             AssigneeGroupCaseStatus entity = toGroupCaseStatusEntity(crGroupCaseStatRequestDto);
@@ -21,20 +27,12 @@ public interface GroupCaseStatusMapper {
             return entity;
         }
 
+    @Mapping(source = "createdBy", target = "createdBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "updatedBy", target = "updatedBy", qualifiedByName = "userIdToFullName")
+    @Mapping(source = "assigneeGroupId", target = "assigneeGroup", qualifiedByName = "assigneeGroupIdToGroupName")
+    @Mapping(source = "currentStatusId", target = "currentStatus", qualifiedByName = "caseStatusIdToStatusName")
+    @Mapping(source = "previousStatusId", target = "previousStatus", qualifiedByName = "caseStatusIdToStatusName")
+    @Mapping(source = "nextStatusId", target = "nextStatus", qualifiedByName = "caseStatusIdToStatusName")
     CrGroupCaseStatResponseDto toCaseStatusResponse(AssigneeGroupCaseStatus assigneeGroupCaseStatus);
-        default CrGroupCaseStatResponseDto toCaseStatusResponseDetails (AssigneeGroupCaseStatus assigneeGroupCaseStatus, String curStatus,
-                                                                 String prevStatus, String nextStatus, String assigneeGroup,
-                                                                 String createdBy, String updatedBy) {
-            CrGroupCaseStatResponseDto responseDto = toCaseStatusResponse(assigneeGroupCaseStatus);
-            responseDto.setCurrentStatus(curStatus);
-            responseDto.setPreviousStatus(prevStatus);
-            responseDto.setNextStatus(nextStatus);
-            responseDto.setAssigneeGroup(assigneeGroup);
-            responseDto.setCreatedBy(createdBy);
-            responseDto.setUpdatedBy(updatedBy);
-            return responseDto;
-        }
-
-
 
 }
--- src/main/java/org/example/incidentmanagement/service/impl/GroupCaseStatusServiceImpl.java
+++ src/main/java/org/example/incidentmanagement/service/impl/GroupCaseStatusServiceImpl.java
@@ -6,7 +6,6 @@ import org.example.incidentmanagement.entity.AssigneeGroupCaseStatus;
 import org.example.incidentmanagement.mappers.GroupCaseStatusMapper;
 import org.example.incidentmanagement.repository.GroupCaseStatusRepository;
 import org.example.incidentmanagement.service.CurrentUserService;
-import org.example.incidentmanagement.service.DefaultConverter;
 import org.example.incidentmanagement.service.GroupCaseStatusService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,17 +16,14 @@ public class GroupCaseStatusServiceImpl implements GroupCaseStatusService {
 
     private GroupCaseStatusRepository groupCaseStatusRepository;
     private GroupCaseStatusMapper groupCaseStatusMapper;
-    private DefaultConverter defaultConverter;
     private CurrentUserService currentUserService;
     Logger logger = LoggerFactory.getLogger(GroupCaseStatusServiceImpl.class);
 
     public GroupCaseStatusServiceImpl(GroupCaseStatusRepository groupCaseStatusRepository,
                                       GroupCaseStatusMapper groupCaseStatusMapper,
-                                      DefaultConverter defaultConverter,
                                       CurrentUserService currentUserService) {
         this.groupCaseStatusRepository = groupCaseStatusRepository;
         this.groupCaseStatusMapper = groupCaseStatusMapper;
-        this.defaultConverter = defaultConverter;
         this.currentUserService = currentUserService;
     }
 
@@ -36,15 +32,7 @@ public class GroupCaseStatusServiceImpl implements GroupCaseStatusService {
         logger.info("Called Find By ID: {}", id);
         AssigneeGroupCaseStatus assigneeGroupCaseStatus = groupCaseStatusRepository.findById(id).orElse(null);
 
-        String createdBy = defaultConverter.getUserFullName(assigneeGroupCaseStatus.getCreatedBy());
-        String updatedBy = defaultConverter.getUserFullName(assigneeGroupCaseStatus.getUpdatedBy());
-        String currentStatus = defaultConverter.getCaseStatusName(assigneeGroupCaseStatus.getCurrentStatusId());
-        String previousStatus = defaultConverter.getCaseStatusName(assigneeGroupCaseStatus.getPreviousStatusId());
-        String nextStatus = defaultConverter.getCaseStatusName(assigneeGroupCaseStatus.getNextStatusId());
-        String assigneeGroup = defaultConverter.getAssigneeGroupName(assigneeGroupCaseStatus.getAssigneeGroupId());
-
-        CrGroupCaseStatResponseDto responseDto = groupCaseStatusMapper.toCaseStatusResponseDetails(assigneeGroupCaseStatus,
-                currentStatus, previousStatus, nextStatus, assigneeGroup, createdBy, updatedBy);
+        CrGroupCaseStatResponseDto responseDto = groupCaseStatusMapper.toCaseStatusResponse(assigneeGroupCaseStatus);
 
         return responseDto;
 
@@ -59,15 +47,7 @@ public class GroupCaseStatusServiceImpl implements GroupCaseStatusService {
 
         groupCaseStatusRepository.save(GroupCaseStatustoSave);
 
-        String createdBy = defaultConverter.getUserFullName(GroupCaseStatustoSave.getCreatedBy());
-        String updatedBy = defaultConverter.getUserFullName(GroupCaseStatustoSave.getUpdatedBy());
-        String currentStatus = defaultConverter.getCaseStatusName(GroupCaseStatustoSave.getCurrentStatusId());
-        String previousStatus = defaultConverter.getCaseStatusName(GroupCaseStatustoSave.getPreviousStatusId());
-        String nextStatus = defaultConverter.getCaseStatusName(GroupCaseStatustoSave.getNextStatusId());
-        String assigneeGroup = defaultConverter.getAssigneeGroupName(GroupCaseStatustoSave.getAssigneeGroupId());
-
-        CrGroupCaseStatResponseDto responseDto = groupCaseStatusMapper.toCaseStatusResponseDetails(GroupCaseStatustoSave,
-                currentStatus, previousStatus, nextStatus, assigneeGroup, createdBy, updatedBy);
+        CrGroupCaseStatResponseDto responseDto = groupCaseStatusMapper.toCaseStatusResponse(GroupCaseStatustoSave);
 
         return responseDto;
 
