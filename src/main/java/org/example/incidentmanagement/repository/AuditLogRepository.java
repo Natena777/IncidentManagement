acda97194f8eda34bcae69ebccbacf10ca481762
Natena777 <nikoloz.natenadze.2@gmail.com>
2026-01-08 23:44:28 +0400

    Add Audit Logging System

---
+++ src/main/java/org/example/incidentmanagement/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package org.example.incidentmanagement.repository;
+
+import org.example.incidentmanagement.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
